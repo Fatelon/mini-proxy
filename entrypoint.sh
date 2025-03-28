fc61abf96e7c78e3790ab4d883388f4af49c904d
Sergei <fatelon@gmail.com>
2025-03-28 16:38:33 +0100

    feat: add logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-exec microsocks -p 1080 -i 0.0.0.0
+echo "[START] $(date) microsocks started"
+exec microsocks -p 1080 -i 0.0.0.0 | while IFS= read -r line; do
+  echo "$(date +'%Y-%m-%d %H:%M:%S') $line"
+done
